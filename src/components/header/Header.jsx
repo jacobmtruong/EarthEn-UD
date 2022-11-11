47bf1d4d6dbe4734d61ecff9252319fd256fd4c2
Jacob Truong <jacob.truong@ymail.com>
2022-11-10 16:24:53 -0800

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import '../header/header.css'
 import pin from '../../images/pin.png'
 import logo from '../../images/whitelogo.png'
@@ -11,11 +11,24 @@ const Header = () => {
                     <img className='pin-img' src={pin} alt="pin-location" />
                     <p>California, United States</p>
                 </div>
+
+                <div>
+                    
+                </div>
             </div>
 
             <div className='header-banner'>
                 <div className='navbar-container'>
-                    <img className='logo-img' src={logo} alt="logo" />
+                    <div className='navbar'>
+                        <Link><img className='logo-img' src={logo} alt="logo" /></Link>
+                        <div className='navbar-list'>
+                            <Link className='nav-item'>Home</Link>
+                            <Link className='nav-item'>About</Link>
+                            <Link className='nav-item'>Solutions</Link>
+                            <Link className='nav-item'>Media</Link>
+                            <Link className='nav-item'>Contact Us</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
