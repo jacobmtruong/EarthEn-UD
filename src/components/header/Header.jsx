87be06ed8f059adf3171d7c6c2d4bd5918636e72
Jacob Truong <jacob.truong@ymail.com>
2022-11-10 20:05:25 -0800

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,34 @@
 import { Link } from 'react-router-dom'
 import '../header/header.css'
-import pin from '../../images/pin.png'
 import logo from '../../images/whitelogo.png'
 
 const Header = () => {
     return (
         <div className='header-container'>
-            <div className='above-banner'>
-                <div className='location-text'>
-                    <img className='pin-img' src={pin} alt="pin-location" />
-                    <p>California, United States</p>
-                </div>
-
+            <div className='navbar-container'>
                 <div>
-                    
+                    <Link><img className='logo-img' src={logo} alt="logo" /></Link>
+                </div>
+                <div className='navbar-list'>
+                    <Link className='nav-item'>Home</Link>
+                    <Link className='nav-item'>About</Link>
+                    <Link className='nav-item'>Solutions</Link>
+                    <Link className='nav-item'>Media</Link>
+                    <Link className='nav-item'>Contact</Link>
                 </div>
             </div>
 
-            <div className='header-banner'>
-                <div className='navbar-container'>
-                    <div className='navbar'>
-                        <Link><img className='logo-img' src={logo} alt="logo" /></Link>
-                        <div className='navbar-list'>
-                            <Link className='nav-item'>Home</Link>
-                            <Link className='nav-item'>About</Link>
-                            <Link className='nav-item'>Solutions</Link>
-                            <Link className='nav-item'>Media</Link>
-                            <Link className='nav-item'>Contact Us</Link>
-                        </div>
-                    </div>
+            <div className='banner-text'>
+                <h1 style={{ fontSize: "60px", color: "white" }}>THE <span style={{ color: "#9aeba6" }}>FUTURE</span> OF <span style={{ color: "#9aeba6" }}>ENERGY</span></h1>
+                <p style={{ fontSize: "30px", color: "white", marginTop: "10px" }}>Enabling Renewable Energy 24/7 for a Carbon-Free Earth</p>
+
+
+                <div className='buttons-group'>
+                    <button className='learnmore-button'>Learn More</button>
+                    <button className='donate-button'>Donate</button>
                 </div>
+
+
             </div>
         </div>
     )
